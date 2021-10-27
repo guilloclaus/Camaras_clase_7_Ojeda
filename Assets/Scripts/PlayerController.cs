a3119971de151347479d2aa8d00a7b395ba16255
GuilleClaus <guillermo.j.ojeda.developer@gmail.com>
2021-10-26 23:34:28 -0300

    Script controlador de PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float Velocidad = 5;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Movimiento();
+    }
+
+    private void Movimiento()
+    {
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Translate(Velocidad * Time.deltaTime * Vector3.left);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Translate(Velocidad * Time.deltaTime * Vector3.right);
+
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(Velocidad * Time.deltaTime * Vector3.forward);
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.Translate(Velocidad * Time.deltaTime * Vector3.back);
+
     }
 }
